8161d5f14c9050d0765873d6a5a582ba4196b188
RanaUniverse <RanaUniverse321@gmail.com>
2025-12-11 22:51:24 +0530

    a simple page to show start my note writing application

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from note-taking-web-app!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+    )
