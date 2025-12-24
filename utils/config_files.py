577613459cc473a9844296038fa38bf967e754f7
RanaUniverse <RanaUniverse321@gmail.com>
2025-12-24 10:31:34 +0530

    i used a logger in a separate module and import this and using this and found this is working now, it need to check

---
+++ utils/config_files.py
@@ -0,0 +1,12 @@
+"""
+This is
+utils/config_files.py
+Here i want to keep some values which is like the
+settings i want to use in my project.
+"""
+
+log_file_name = "web_app.log"
+
+# I did this below if true it will shows log in console
+# otherwise if false it will not shows in console
+enable_console_logging: bool = True
---
+++ utils/custom_logger.py
@@ -0,0 +1,38 @@
+"""
+This is utils.custom_logger.py file
+I make this for checking how this will work
+"""
+
+import logging
+
+from utils.config_files import log_file_name, enable_console_logging
+
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    fmt="{asctime} - {levelname} - {name} - {filename} - {lineno} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+file_handler = logging.FileHandler(
+    filename=log_file_name,
+    mode="a",
+    encoding="utf-8",
+)
+logger.addHandler(file_handler)
+file_handler.setFormatter(formatter)
+
+
+if enable_console_logging:
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
+    console_handler.setFormatter(formatter)
+
+
+if __name__ == "__main__":
+    logger.debug("Somethings Happens")
